37caf730b29ae6679422fdd620f5e00b83bd196a
soligloo <solene@ligloo.net>
2017-01-17 15:21:58 +0100

    Essai modif servername 17/01/17
    Je change localhost par localhost:8888 pour tester Github.

--- acces_bdd.php
+++ acces_bdd.php
@@ -15,7 +15,7 @@ function debug_sql($sql, $message=NULL) {
 }
 
 function ouvre_bdd() {
-	$servername = "localhost";
+	$servername = "localhost:8888";
 	$username = "root";
 	$password = "root";
 	$dbname = "Test";
